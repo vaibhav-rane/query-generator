6b3c870fa59e730731dfc4d4dcdd97b7c865e62c
varane <vaibhav.rane@copart.com>
2022-06-30 22:24:09 -0400

    Initial commit

---
+++ src/main/java/com/custom/orm/querygenerator/QueryGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.custom.orm.querygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/custom/orm/querygenerator/QueryGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom.orm.querygenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
